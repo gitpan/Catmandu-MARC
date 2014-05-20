2cfde93810622b5bbbaab74bf2c42bff58edfe37
Patrick Hochstenbach <patrick.hochstenbach@gmail.com>
2014-05-20 08:11:40 +0000

    Import of HOCHSTEN/Catmandu-MARC-0.114 from CPAN.
    gitpan-cpan-distribution: Catmandu-MARC
    gitpan-cpan-version:      0.114
    gitpan-cpan-path:         HOCHSTEN/Catmandu-MARC-0.114.tar.gz
    gitpan-cpan-author:       HOCHSTEN
    gitpan-cpan-maturity:     released

--- lib/Catmandu/Fix/marc_in_json.pm
+++ lib/Catmandu/Fix/marc_in_json.pm
@@ -2,14 +2,9 @@ package Catmandu::Fix::marc_in_json;
 
 use Catmandu::Sane;
 use Moo;
+use Catmandu::Fix::Has;
 
-has opts => (is => 'ro');
-
-around BUILDARGS => sub {
-    my ($orig, $class, %opts) = @_;
-    $opts{-record} ||= 'record';
-    $orig->($class, opts => \%opts);
-};
+has record => (fix_opt => 1);
 
 # Transform a raw MARC array into MARC-in-JSON
 # See Ross Singer work at:
@@ -17,7 +12,7 @@ around BUILDARGS => sub {
 sub fix {
     my ($self, $data) = @_;
 
-    my $marc_pointer = $self->opts->{-record};
+    my $marc_pointer = $self->record // 'record';
 
     if (my $marc = delete $data->{$marc_pointer}) {
         for my $field (@$marc) {
@@ -53,7 +48,10 @@ Catmandu::Fix::marc_in_json - transform a Catmandu MARC record into MARC-in-JSON
 =head1 SYNOPSIS
 
    # Create a deeply nested key
-   marc_in_json();
+   marc_in_json()
+
+   # Optionally provide a pointer to the marc record
+   marc_in_json(-record => 'record')
 
 =head1 SEE ALSO
 
--- lib/Catmandu/Fix/marc_remove.pm
+++ lib/Catmandu/Fix/marc_remove.pm
@@ -2,22 +2,17 @@ package Catmandu::Fix::marc_remove;
 
 use Catmandu::Sane;
 use Moo;
+use Catmandu::Fix::Has;
 
-has marc_tag => (is => 'ro', required => 1);
-has record_key => (is => 'ro', default => sub { "record" });
-
-around BUILDARGS => sub {
-    my ($orig, $class, $marc_tag, %opts) = @_;
-    my $attrs = { marc_tag => $marc_tag };
-    $attrs->{record_key} = $opts{-record} if defined $opts{-record};
-    $orig->($class, $attrs);
-};
+has marc_tag => (fix_arg => 1);
+has record   => (fix_opt => 1);
 
 sub fix {
     my ($self, $data) = @_;
-    my $marc_tag = $self->marc_tag;
+    my $marc_tag   = $self->marc_tag;
+    my $record_tag = $self->record // 'record';
 
-    if (my $marc = $data->{$self->record_key}) {
+    if (my $marc = $data->{$record_tag}) {
         for (my $i = @$marc - 1; $i >= 0; $i--) {
             if ($marc->[$i][0] eq $marc_tag) {
                 splice @$marc, $i, 1;
@@ -35,10 +30,10 @@ Catmandu::Fix::marc_remove - remove marc fields
 =head1 SYNOPSIS
 
     # remove all marc 600 fields
-    marc_remove('600');
+    marc_remove('600')
 
     # the same with the marc fields in 'record2'
-    marc_remove('600', '-record', 'record2');
+    marc_remove('600', '-record', 'record2')
 
 =head1 SEE ALSO
 
